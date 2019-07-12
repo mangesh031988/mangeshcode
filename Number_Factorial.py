50903eb9e15abc3274bd85dc04c4e6f9e73ac296
mangesh031988 <50075575+mangesh031988@users.noreply.github.com>
2019-07-12 22:21:27 +0530

    Number_Factorial.py
    Calculate Factorial for the input number

---
+++ Number_Factorial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 00:07:06 2019
+
+Calculate Factorial for the input number
+
+@author: Mangesh
+"""
+
+## Factorial 5!=5*4*3*2*1 = 120
+
+#Declare
+number = 9
+
+#Factorial
+factorial = 1
+
+if number < 0:
+    print("Factorial does not exists for -Ve numbers")
+elif number == 0:
+    print("The Factorial for 0 is 1")
+else:
+    for i in range(1,number + 1):
+        factorial = factorial*i
+    print("The Factorial for", number, "is",factorial )
+    
