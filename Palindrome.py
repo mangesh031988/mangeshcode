740dbbc89b42f8a54f450b0f21b6c8d9ee4f66fe
mangesh031988 <50075575+mangesh031988@users.noreply.github.com>
2019-07-12 22:26:06 +0530

    Find the given number is Palindrome number or not
    Design a code which will find the given number is Palindrome number or not.

---
+++ Palindrome.py
@@ -0,0 +1,25 @@
+
+
+"""
+Created on Mon Jul  8 00:22:44 2019
+
+Question 5.Design a code which will find the given number is Palindrome number or not.
+
+@author: Mangesh
+"""
+
+abc = int(input("enter a number: "))
+ 
+xyz = abc
+rev = 0
+ 
+while xyz != 0:
+	rev = (rev * 10) + (xyz % 10)
+	xyz = xyz // 10
+ 
+if abc == rev:
+	print("number is palindrome")
+else:
+	print("number is not palindrome")
+
+
