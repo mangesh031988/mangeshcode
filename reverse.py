e9edaf67bb389d9f4a251d326b8f01de2974e94a
mangesh031988 <50075575+mangesh031988@users.noreply.github.com>
2019-07-13 14:55:30 +0530

    Python Program to reverse input string
    Please write a program which accepts a string from console and print it in reverse order

---
+++ reverse.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  12 00:17:13 2019
+
+Question 6 : Please write a program which accepts a string from console and print it in reverse order
+
+@author: Mangesh
+"""
+
+string = input("Enter the string : ")
+
+rev_str = string[::-1] 
+
+print("Your string : ", string)
+print("Reverse string : ", rev_str)
